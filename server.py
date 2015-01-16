37dc8489bcd5e38726ab861555274509263e2140
bread-on-toast <andre@allthefish.de>
2015-01-16 23:01:08 +0100

    geändert:               server.py

--- server.py
+++ server.py
@@ -14,20 +14,12 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
     """
 
     def handle(self):
-        # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
-        #print "{} wrote:".format(self.client_address[0])
-        #print self.data
-        # just send back the same data, but upper-cased
 	phrase=("<meta http-equiv='content-type' content='text/html; charset=UTF-8'>Ich fordere: Wer %s, der %s!" % (g.generateWordPair())).encode('utf8', 'replace')
         self.request.sendall(phrase)
 	print phrase
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
-    # Create the server, binding to localhost on port 9999
     server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
-
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
     server.serve_forever()
